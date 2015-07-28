06fd757fa2b528c3d96922b05eba57d5da897acd
tomas-mm <tomasmontserrat@gmail.com>
2015-07-28 12:52:47 +0200

    Add non-recursive DFS implementation to handle big graphs without recursion or stack problems. Also report the 5 largest sections found.

--- scc/kosaraju.py
+++ scc/kosaraju.py
@@ -4,10 +4,57 @@ Kosaraju's 2-pass algorithm
 """
 
 
+import sys
 import argparse
+import random
 
 
 def dfs(edges, verts, new_verts_order, explored_verts,
+        sections, leader, i, reverse=False):
+    """
+    Depth-First Search from vertex i (non-recursive implementation)
+    Recursive implementation would fail for big data sets on systems with
+    limited memory (even increasing the recursion limit)
+    Modifies new_verts_order, explored_verts and sections
+    """
+    execution_stack = [i]
+
+    while len(execution_stack) > 0:
+        v = execution_stack.pop()
+
+        if isinstance(v, tuple):
+            # finish mark
+            new_verts_order.append(v[0])
+        else:
+            if v in explored_verts:  # we reached here through another path before
+                continue
+
+            # set leader and mark as explored
+            explored_verts[v] = leader
+            try:
+                sections[leader].append(v)
+            except KeyError:
+                sections[leader] = [v]
+
+            # debug
+            if random.randint(1, 100) == 1:
+                l_explored = len(explored_verts)
+                l_verts = len(verts)
+                print "Explored %s of %s (%s%%)" % (l_explored, l_verts, float(l_explored) / l_verts * 100)
+
+            execution_stack.append((v,))  # finish mark
+
+            for e in verts[v]:
+                arc = list(edges[e])
+                if reverse:
+                    arc.reverse()
+                if arc[0] == v:
+                    # outgoing arc
+                    if arc[1] not in explored_verts:
+                        execution_stack.append(arc[1])
+
+
+def dfs_recursive(edges, verts, new_verts_order, explored_verts,
         sections, leader, i, reverse=False):
     """
     Depth-First Search from vertex i
@@ -20,6 +67,11 @@ def dfs(edges, verts, new_verts_order, explored_verts,
     except KeyError:
         sections[leader] = [i]
 
+    # debug
+    if random.randint(1, 100) == 1:
+        l_explored = len(explored_verts)
+        l_verts = len(verts)
+        print "Explored %s of %s (%s%%)" % (l_explored, l_verts, float(l_explored) / l_verts * 100)
 
     for e in verts[i]:
         arc = list(edges[e])
@@ -28,7 +80,7 @@ def dfs(edges, verts, new_verts_order, explored_verts,
         if arc[0] == i:
             # outgoing arc
             if arc[1] not in explored_verts:
-                dfs(edges, verts, new_verts_order, explored_verts,
+                dfs_recursive(edges, verts, new_verts_order, explored_verts,
                     sections, leader, arc[1], reverse)
 
     new_verts_order.append(i)
@@ -48,6 +100,9 @@ def dfs_loop(edges, verts, verts_order, reverse=False):
 
     for s in reversed(verts_order):
         if s not in explored_verts:
+            # dfs_recursive(edges, verts, new_verts_order, explored_verts,
+            #              sections, s, s, reverse)
+
             dfs(edges, verts, new_verts_order, explored_verts,
                 sections, s, s, reverse)
 
@@ -93,7 +148,7 @@ def get_adjacency_lists_directed(in_file):
     verts = {}
     edge_count = 0
     with open(in_file) as f:
-        for line in f.readlines():
+        for line in f:
             vertex = line.split()
             v1 = int(vertex[0])
             for v2_s in vertex[1:]:
@@ -119,8 +174,21 @@ def get_adjacency_lists_directed(in_file):
 if __name__ == '__main__':
     args = parse_args()
     edges, verts = get_adjacency_lists_directed(args.file)
+
+    # Use a bigger recursion limit for big data (in unix systems new system
+    # ulimit would also have to be applied).
+    # This seems not work in Windows (use non-recursive approach there)
+    recursion_limit = max(sys.getrecursionlimit(), len(verts))
+    sys.setrecursionlimit(recursion_limit)
+    print 'recursion limit set to %s' % recursion_limit
+
     sections = scc(edges, verts)
 
-    print "Strongly Connected Sections: %s" % sections
+    # getting the 5 largest sections
+    section_sizes = [(k, len(v)) for k, v in sections.iteritems()]
+    section_sizes.sort(key=lambda x: x[1], reverse=True)
+
+    # print "Strongly Connected Sections: %s" % sections
     print "Number of Strongly Connected Sections: %s" % len(sections)
+    print "Largest sections: %s" % section_sizes[:5]
 
