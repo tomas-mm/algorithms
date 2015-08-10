87b8a466170198b5071e42430ac8446fe260828f
tomas-mm <tomasmontserrat@gmail.com>
2015-08-10 21:17:23 +0200

    Faster implementation of MedianMaintainer checking heap lengths before pop and making use of heap property that the min value is always stored in the first position of the array.

--- data_structures/median_maintenance.py
+++ data_structures/median_maintenance.py
@@ -1,14 +1,18 @@
 """
 "Median Maintenance" algorithm based on 2 heaps
-given a stream of numbers keep always the median of them
+given a stream of numbers keep always the median
+of them when a new one is added.
 """
 
 import argparse
 import heapq
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description=("Input file is treated as a stream of numbers and the median "
-                                                  "value when each one of them is received is accumulated (result)"))
+    parser = argparse.ArgumentParser(
+        description=("Input file is treated as a stream of numbers and the median "
+                     "value when each one of them is received is accumulated (result)")
+    )
     parser.add_argument(
         'file',
         help=('input file, one integer per row')
@@ -23,72 +27,58 @@ def parse_args():
 
 
 class MedianMaintainer(object):
+    """
+    Class to maintain the median of a stream of numbers using two heaps
+    """
     def __init__(self):
         self.max_heap = []
         self.min_heap = []
-
-    def _push_3_values(self, a, b, c):
-        """
-        a < b < c
-        """
-        if len(self.max_heap) > len(self.min_heap):
-            # as we only push a lower value to max_heap and the former
-            # max is moved to right, we must retrieve the next max in
-            # max_heap to retuen the current median
-            heapq.heappush(self.max_heap, -a)
-            heapq.heappush(self.min_heap, b)
-            heapq.heappush(self.min_heap, c)
-            median = -heapq.heappop(self.max_heap)
-            heapq.heappush(self.max_heap, -median)
-            return median
-        else:
-            heapq.heappush(self.max_heap, -a)
-            heapq.heappush(self.max_heap, -b)
-            heapq.heappush(self.min_heap, c)
-            return b
 
     def push(self, value):
         """
         store a new value and return the current median
         """
-        # the max value of max_heap should be the median
+
+        l_max = len(self.max_heap)
+        l_min = len(self.min_heap)
+
         try:
-            max_of_left = -heapq.heappop(self.max_heap)
+            max_of_left = -self.max_heap[0]
+            min_of_right = self.min_heap[0]
         except IndexError:
-            max_of_left = None
-        try:
-            min_of_right = heapq.heappop(self.min_heap)
-        except IndexError:
-            min_of_right = None
-
-        if max_of_left and min_of_right:
-            # Standard case, 3 pushes to distribute
-            if max_of_left < value < min_of_right:
-                res = self._push_3_values(max_of_left, value, min_of_right)
-            elif value < max_of_left:
-                res = self._push_3_values(value, max_of_left, min_of_right)
-            else:
-                res = self._push_3_values(max_of_left, min_of_right, value)
-            return res
-
-        # Handle corner cases when the heaps are empty
-        elif min_of_right is None and max_of_left is None:
-            heapq.heappush(self.max_heap, -value)
-            return value
-
-        elif min_of_right is None:
-            if value < max_of_left:
+            # Handle initialization cases with both heaps empty or min_heap only empty
+            if  l_max == 0:
                 heapq.heappush(self.max_heap, -value)
-                heapq.heappush(self.min_heap, max_of_left)
                 return value
+
+            if l_min == 0:
+                max_of_left = -heapq.heappop(self.max_heap)
+                if value > max_of_left:
+                    heapq.heappush(self.max_heap, -max_of_left)
+                    heapq.heappush(self.min_heap, value)
+                    return max_of_left
+                else:
+                    heapq.heappush(self.max_heap, -value)
+                    heapq.heappush(self.min_heap, max_of_left)
+                    return value
+
+        if l_max == l_min:
+            if value <= min_of_right:
+                heapq.heappush(self.max_heap, -value)
             else:
-                heapq.heappush(self.max_heap, -max_of_left)
+                min_of_right = heapq.heappop(self.min_heap)
                 heapq.heappush(self.min_heap, value)
-                return max_of_left
+                heapq.heappush(self.max_heap, -min_of_right)
+        else:
+            # l_max > l_min
+            if value >= min_of_right:
+                heapq.heappush(self.min_heap, value)
+            else:
+                heapq.heappush(self.max_heap, -value)
+                max_of_left = -heapq.heappop(self.max_heap)
+                heapq.heappush(self.min_heap, max_of_left)
 
-
-        else:  # max_of_left is None
-            raise('ERROR this case should NEVER happen. We start filling max_heap always')
+        return -self.max_heap[0]
 
 
 def main():
